173cec42b618d66558b513f1e33adbce271dcabf
Mukundan314 <mukundan314@gmail.com>
2022-01-23 17:37:25 +0000

    Deploying to gh-pages from @ Mukundan314/dcall@51538127bd8c6c2f8ec563ed96e543c9bb286dad 🚀

---
+++ precache-manifest.ab6f6514eb8dee45fdb6f73792ba376e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8fc44603d758b6d85e7",
+    "url": "css/app.3b72bb5a.css"
+  },
+  {
+    "revision": "55808906dee47bdec1a48fd8e0b93d52",
+    "url": "index.html"
+  },
+  {
+    "revision": "e8fc44603d758b6d85e7",
+    "url": "js/app.9ebc1b0f.js"
+  },
+  {
+    "revision": "b8d32231d2f8d745b131",
+    "url": "js/chunk-vendors.6a05bd73.js"
+  },
+  {
+    "revision": "fb26fd36836eaf69e9a8acd261c00bbe",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
