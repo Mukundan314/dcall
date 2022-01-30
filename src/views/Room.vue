7e62a27061e77b4b2e7410928ecd745417d5f95a
Mukundan314 <mukundan314@gmail.com>
2022-01-30 22:35:08 +0530

    add stun server

--- src/views/Room.vue
+++ src/views/Room.vue
@@ -153,7 +153,9 @@ export default defineComponent({
     },
 
     connect(target: string, offerer: boolean) {
-      const peerConnection = new RTCPeerConnection();
+      const peerConnection = new RTCPeerConnection({
+        iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
+      });
 
       this.connections.get(target)?.close();
       this.connections.set(target, peerConnection);
