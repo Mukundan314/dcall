3317ff05f2d7906955c704cc6ed37baa73df3579
LostButton <71197188+LostButton@users.noreply.github.com>
2022-10-06 08:42:29 -0400

    fixing shadow

--- src/App.vue
+++ src/App.vue
@@ -28,12 +28,15 @@ body {
   background-color: #444546;
 }
 
+/* to keep glowing effect box shadow must be there*/
 .searchbar {
   align-items:center;
   padding: 10px;
   width: 250px;
   border: solid #c8ccd0 1px;
   border-radius: 5px 0px 0px 5px;
+  -webkit-transition: box-shadow linear 0.2s;
+  transition: box-shadow linear 0.2s, width 500ms ease;
 }
 
 .searchbar:focus {
@@ -44,6 +47,7 @@ body {
 
 .searchbar:hover {
   border-color: #9ecaed;
+  box-shadow: 0 0 10px #9ecaed;
   width: 280px;
 }
 
